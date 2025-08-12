03b8ccf2b4611328f95454dd4963ffb931b54c07
m-ert <mertekincmp@gmail.com>
2025-08-13 00:46:47 +0300

    tried adding the loop over csv list

--- mail.py
+++ mail.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.message import EmailMessage
 import json
+import csv
+import time
 
 with open("config.json") as f:
     config = json.load(f)
@@ -16,9 +18,7 @@ with open(textfile) as fp:
 
 # me == the sender's email address
 # you == the recipient's email address
-msg['Subject'] = f'The contents of {textfile}'
 msg['From'] = config["sender"]
-msg['To'] = config["receiver"]
 
 my_email = config["gmail_user"]
 app_password= config["gmail_pass"]
@@ -35,6 +35,16 @@ my_server.starttls()
 
 # Login with your email and password
 my_server.login(my_email, app_password)
+
+with open('list.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for lines in csvFile:
+        cur_address = lines[0]
+        msg['Subject'] = f"hello {cur_address}"
+        msg['To'] = cur_address
+        my_server.send_message(msg)
+        print(f"mail sent to {cur_address} successfully")
+
 
 my_server.send_message(msg)
 my_server.quit()
