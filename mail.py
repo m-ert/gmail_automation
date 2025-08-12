d645c502e57492bc21000669830c8cfc8324ffe2
m-ert <mertekincmp@gmail.com>
2025-08-13 00:51:41 +0300

    fixed the looping over .csv file to create a new EmailMessage each time

--- mail.py
+++ mail.py
@@ -8,17 +8,7 @@ with open("config.json") as f:
     config = json.load(f)
 
 # Open the plain text file whose name is in textfile for reading.
-
 textfile = "/home/mhsn/Desktop/automation/test.txt"
-
-with open(textfile) as fp:
-    # Create a text/plain message
-    msg = EmailMessage()
-    msg.set_content(fp.read())
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['From'] = config["sender"]
 
 my_email = config["gmail_user"]
 app_password= config["gmail_pass"]
@@ -26,7 +16,6 @@ app_password= config["gmail_pass"]
     # SMTP Server and port no for GMAIL.com
 gmail_server= "smtp.gmail.com"
 gmail_port= 587
-
 
     # Starting connection
 my_server = smtplib.SMTP(gmail_server, gmail_port)
@@ -36,15 +25,21 @@ my_server.starttls()
 # Login with your email and password
 my_server.login(my_email, app_password)
 
-with open('list.csv', mode ='r')as file:
-  csvFile = csv.reader(file)
-  for lines in csvFile:
+with open('list.csv', mode='r') as file:
+    csvFile = csv.reader(file)
+    for lines in csvFile:
         cur_address = lines[0]
-        msg['Subject'] = f"hello {cur_address}"
+
+        # Create new message per recipient
+        msg = EmailMessage()
+        with open(textfile) as fp:
+            msg.set_content(fp.read())
+
+        msg['From'] = config["sender"]
         msg['To'] = cur_address
+        msg['Subject'] = f"hello {cur_address}"
+
         my_server.send_message(msg)
-        print(f"mail sent to {cur_address} successfully")
+        print(f"Mail sent to {cur_address} successfully")
 
-
-my_server.send_message(msg)
 my_server.quit()
