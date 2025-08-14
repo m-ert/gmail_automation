74c5b2f8628f462f21ee2917cce0a8a4dd01d1a9
m-ert <mertekincmp@gmail.com>
2025-08-14 16:37:05 +0300

    added attachment function

--- mail.py
+++ mail.py
@@ -3,12 +3,24 @@ from email.message import EmailMessage
 import json
 import csv
 import time
+import mimetypes
 
 with open("config.json") as f:
     config = json.load(f)
 
 # Open the plain text file whose name is in textfile for reading.
 textfile = "/home/mhsn/Desktop/automation/test.txt"
+resume = "/home/mhsn/cv/Muhsin_Ertekin.pdf"
+
+ctype, encoding = mimetypes.guess_type(resume)
+if ctype is None or encoding is not None:
+    ctype = 'application/octet-stream'  # default type for unknown files
+
+maintype, subtype = ctype.split('/', 1)
+
+with open(resume, 'rb') as f:
+    resume_data = f.read()
+
 
 my_email = config["gmail_user"]
 app_password= config["gmail_pass"]
@@ -37,7 +49,8 @@ with open('list.csv', mode='r') as file:
 
         msg['From'] = config["sender"]
         msg['To'] = cur_address
-        msg['Subject'] = f"hello {cur_address}"
+        msg['Subject'] = f"Staj Başvurusu"
+        msg.add_attachment(resume_data, maintype=maintype, subtype=subtype, filename="Muhsin_Ertekin.pdf")
 
         my_server.send_message(msg)
         print(f"Mail sent to {cur_address} successfully")
